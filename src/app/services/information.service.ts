6bd025469f78352a207e0476f60ec77e30d2a117
WilverArriaza <clixnetcom@gmail.com>
2023-11-10 00:17:22 -0600

    Firebase service done

--- src/app/services/information.service.ts
+++ src/app/services/information.service.ts
@@ -7,14 +7,32 @@ import { HttpClient } from '@angular/common/http'
 export class InformationService {
 
   info:any = {};
+  load:boolean = false;
+  load_about:boolean= false;
+  team:any = [];
 
   constructor( public http:HttpClient ) { 
+    this.load_info();
+    this.load_about_us();
+  }
 
+  public load_info(){
     this.http.get("assets/data/info.page.json")
       .subscribe( data => {
-        console.log(data);
+        // console.log(data);
+        this.load = true;
         this.info = data;
       } )
 
+  }
+
+  public load_about_us(){
+
+    this.http.get("https://testportfolio-400e2-default-rtdb.firebaseio.com/team.json")
+      .subscribe( data => {
+        console.log(data);
+        this.load_about = true;
+        this.team = data;
+      } )
   }
 }
